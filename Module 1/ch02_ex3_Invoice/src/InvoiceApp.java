65fe04ff0c660a5830195b190581c59b8c125970
t-ryles <tajhakeem.ryles@gmail.com>
2023-06-15 22:05:11 -0500

    Module 2: Update MPG invoice.

--- Module 1/ch02_ex3_Invoice/src/InvoiceApp.java
+++ Module 1/ch02_ex3_Invoice/src/InvoiceApp.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 
+
+/*
+* Taj-Hakeem Ryles
+* INFO 1521 WW
+* Invoice Application
+* Jayson McCune
+* June 7, 2023
+*/
+
 public class InvoiceApp {
 
-    public static <Int> void main(String[] args) {
+    public static void main(String[] args) {
         // welcome the user to the program
         System.out.println("Welcome to the Invoice Total Calculator");
         System.out.println();  // print a blank line
@@ -10,13 +19,16 @@ public class InvoiceApp {
         // create a Scanner object named sc
         Scanner sc = new Scanner(System.in);
 
-        // perform invoice calculations until choice isn't equal to "y" or "Y"
+        // Initializing choice variable to Y
         String choice = "y";
+
+        // Initializing variables for later reference
         int invoiceCount = 0;
         double invoiceAverage = 0.00;
         double averageDiscount = 0.00;
 
-        while (choice.equalsIgnoreCase("y")) {
+        // perform invoice calculations until choice is equal to "N" or "n"
+        while (!choice.equalsIgnoreCase("N")) {
             // get the invoice subtotal from the user
             System.out.print("Enter subtotal:   ");
             String input = sc.nextLine();
@@ -24,7 +36,9 @@ public class InvoiceApp {
 
             // calculate the discount amount and total
             double discountPercent;
-            if (subtotal >= 200) {
+            if(subtotal >= 500){
+                discountPercent = .25;
+            } else if (subtotal >= 200) {
                 discountPercent = .2;
             } else if (subtotal >= 100) {
                 discountPercent = .1;
@@ -50,10 +64,11 @@ public class InvoiceApp {
             choice = sc.nextLine();
             System.out.println();
         }
+        //Displaying information when N is entered
         if (choice.equalsIgnoreCase("N")) {
             String nMessage = "Number of invoices: " + invoiceCount + "\n"
-                            + "Average invoice:  " + invoiceAverage + "\n"
-                            + "Average discount:    " + averageDiscount + "\n";
+                            + "Average invoice:    " + invoiceAverage + "\n"
+                            + "Average discount:   " + averageDiscount + "\n";
 
             System.out.println(nMessage);
         }
