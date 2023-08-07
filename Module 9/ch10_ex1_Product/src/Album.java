44d9e75140dfee4163dc8592d370b9552457d9d9
t-ryles <tajhakeem.ryles@gmail.com>
2023-08-06 21:16:13 -0500

    Module 9 -First comment

---
+++ Module 9/ch10_ex1_Product/src/Album.java
@@ -0,0 +1,16 @@
+public class Album extends Product {
+
+    private String artist;
+    public Album() {
+        super();
+        artist = "";
+        count++;
+    }
+
+    public void setArtist (String artist) { this.artist = artist; }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + artist;
+    }
+}
